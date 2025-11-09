6336d3cc20fdbfec6e932c36a5addccaead0d9cf
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-08 20:25:31 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Hello everyone");
+
+let button = document.querySelector(".button");
+let body = document.body;
+
+
+button.addEventListener("click", () => {
+    if (button.innerText === "Ciekawostka") {
+        button.innerText = "Każde arcydzieło, które tworzę, ma swoją małą historię i duszę.";
+        button.style.backgroundColor = "#94a728ff"; 
+    } else {
+        button.innerText = "Ciekawostka";
+        button.style.backgroundColor = "#3b7e3bff"; 
+    }
+});
